4781bbcb261d25b32051a8574c5ea30fdc26ac94
Lucas Rodrix <lucasrodrigues1985.lfr@gmail.com>
2020-06-24 12:50:04 -0300

    Corrigindo a configuraçao do PHPMailer

--- processa_envio.php
+++ processa_envio.php
@@ -43,42 +43,49 @@
 	if(!$mensagem->mensagemValida()){
 		echo 'Mensagem não é Válida';
 		die();
-		// header('Location:index.php');
 	}
 
-	$mail = new PHPMailer(true);
+	// Instantiation and passing `true` enables exceptions
+	$mail = new PHPMailer;
 
-	try {
-	    //Server settings
-	    $mail->SMTPDebug = 2;										// Enable verbose debug output
-	    $mail->isSMTP();                                            // Send using SMTP
-	    $mail->Host       = 'smtp.gmail.com';                    	// Set the SMTP server to send through
-	    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-	    $mail->Username   = 'lucasrodrigues1985.lfr@gmail.com';     // SMTP username
-	    $mail->Password   = 'lucas@1115';                           // SMTP password
-	    $mail->SMTPSecure = 'tls';         							// Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-	    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+		//Server settings
+		// $mail->SMTPDebug = 4;                      // Enable verbose debug output
+		$mail->isSMTP();                                            // Send using SMTP
+		$mail->Host = gethostbyname('smtp.gruporodrix.net');                    // Set the SMTP server to send through
+		$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+		$mail->Username   = 'lucas.rodrigues=gruporodrix.net';                     // SMTP username
+		$mail->Password   = 'Renata.1983';                               // SMTP password
+		$mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+		$mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+		$mail->SMTPOptions = array(
+			'ssl' => array(
+				'verify_peer' => false,
+				'verify_peer_name' => false,
+				'allow_self_signed' => true
+			)
+		);
+		//Recipients
+		$mail->setFrom('lucas.rodrigues@gruporodrix.net', 'Mailer');
+		$mail->addAddress('lucas.rodrigues@gruporodrix.net', 'Luck User');     // Add a recipient
+		// $mail->addAddress('ellen@example.com');               // Name is optional
+		// $mail->addReplyTo('info@example.com', 'Information');
+		// $mail->addCC('cc@example.com');
+		// $mail->addBCC('bcc@example.com');
 
-	    //Recipients
-	    $mail->setFrom('lucasrodrigues1985.lfr@gmail.com', 'Web Completo Remetente');
-	    $mail->addAddress('lucasrodrigues1985.lfr@gmail.com', 'Web Completo Destinatário');     // Add a recipient
-	    // $mail->addAddress('ellen@example.com');               // Name is optional
-	    // $mail->addReplyTo('info@example.com', 'Information');
-	    // $mail->addCC('cc@example.com');
-	    // $mail->addBCC('bcc@example.com');
+		// Attachments
+		// $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+		// $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
-	    // Attachments
-	    // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-	    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+		// Content
+		$mail->isHTML(true);                                  // Set email format to HTML
+		
+		$mail->Subject = 'Here is the subject';
+		$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+		$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-	    // Content
-	    $mail->isHTML(true);                                  // Set email format to HTML
-		$mail->Subject = $mensagem->__get('assunto');
-		$mail->Body    = $mensagem->__get('mensagem');
-	    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-	    $mail->send();
-	    echo 'Message has been sent';
-	} catch (Exception $e) {
-	    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-	}
+		if(!$mail->send()) {
+			echo 'Message could not be sent.';
+			echo 'Mailer Error: ' . $mail->ErrorInfo;
+		} else {
+			echo 'Message has been sent';
+		}
