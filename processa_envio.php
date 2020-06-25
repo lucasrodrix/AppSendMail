d1f800ab2998c46d25b2ed61cd5261cd79961673
Lucas Rodrix <lucasrodrigues1985.lfr@gmail.com>
2020-06-25 10:11:24 -0300

    Enviando emails com base nos parametros do FrontEnd

--- processa_envio.php
+++ processa_envio.php
@@ -38,7 +38,7 @@
 	$mensagem->__set('assunto', $_POST['assunto']);
 	$mensagem->__set('mensagem', $_POST['mensagem']);
 
-	// print_r($mensagem);
+	print_r($mensagem);
 
 	if(!$mensagem->mensagemValida()){
 		echo 'Mensagem não é Válida';
@@ -49,13 +49,13 @@
 	$mail = new PHPMailer;
 
 		//Server settings
-		// $mail->SMTPDebug = 4;                      // Enable verbose debug output
+		// $mail->SMTPDebug = 4;                      				// Enable verbose debug output
 		$mail->isSMTP();                                            // Send using SMTP
-		$mail->Host = gethostbyname('smtp.gruporodrix.net');                    // Set the SMTP server to send through
+		$mail->Host = gethostbyname('smtp.gruporodrix.net');       	// Set the SMTP server to send through
 		$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-		$mail->Username   = 'lucas.rodrigues=gruporodrix.net';                     // SMTP username
-		$mail->Password   = 'Renata.1983';                               // SMTP password
-		$mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+		$mail->Username   = 'lucas.rodrigues=gruporodrix.net';      // SMTP username
+		$mail->Password   = 'Renata.1983';                          // SMTP password
+		$mail->SMTPSecure = 'tls';         							// Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 		$mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 		$mail->SMTPOptions = array(
 			'ssl' => array(
@@ -65,8 +65,8 @@
 			)
 		);
 		//Recipients
-		$mail->setFrom('lucas.rodrigues@gruporodrix.net', 'Mailer');
-		$mail->addAddress('lucas.rodrigues@gruporodrix.net', 'Luck User');     // Add a recipient
+		$mail->setFrom('lucas.rodrigues@gruporodrix.net', 'Rodrix Mailer');
+		$mail->addAddress($mensagem->__get('para'));     // Add a recipient
 		// $mail->addAddress('ellen@example.com');               // Name is optional
 		// $mail->addReplyTo('info@example.com', 'Information');
 		// $mail->addCC('cc@example.com');
@@ -79,9 +79,9 @@
 		// Content
 		$mail->isHTML(true);                                  // Set email format to HTML
 		
-		$mail->Subject = 'Here is the subject';
-		$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-		$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+		$mail->Subject = $mensagem->__get('assunto');
+		$mail->Body    =  $mensagem->__get('mensagem');
+		// $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
 		if(!$mail->send()) {
 			echo 'Message could not be sent.';
